37850eaed66a6f5401f3309f7f519db585f893df
Choudhary <rohitchoudhary.git@gmail.com>
2019-06-29 21:25:36 +0530

    v

--- model.py
+++ model.py
@@ -81,58 +81,24 @@ def build_model(args):
 
 
 def train_model(model, args, X_train, X_valid, y_train, y_valid):
-    """
-    Train the model
-    """
-    #Saves the model after every epoch.
-    #quantity to monitor, verbosity i.e logging mode (0 or 1), 
-    #if save_best_only is true the latest best model according to the quantity monitored will not be overwritten.
-    #mode: one of {auto, min, max}. If save_best_only=True, the decision to overwrite the current save file is
-    # made based on either the maximization or the minimization of the monitored quantity. For val_acc, 
-    #this should be max, for val_loss this should be min, etc. In auto mode, the direction is automatically
-    # inferred from the name of the monitored quantity.
     checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
                                  save_best_only=args.save_best_only,
                                  mode='auto')
-
-    #calculate the difference between expected steering angle and actual steering angle
-    #square the difference
-    #add up all those differences for as many data points as we have
-    #divide by the number of them
-    #that value is our mean squared error! this is what we want to minimize via
-    #gradient descent
     model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
-
-    #Fits the model on data generated batch-by-batch by a Python generator.
-
-    #The generator is run in parallel to the model, for efficiency. 
-    #For instance, this allows you to do real-time data augmentation on images on CPU in 
-    #parallel to training your model on GPU.
-    #so we reshape our data into their appropriate batches and train our model simulatenously
-    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
+    model.fit_generator(batch_generator('./data/IMG/', X_train, y_train, args.batch_size, True),
                         args.samples_per_epoch,
                         args.nb_epoch,
                         max_q_size=1,
-                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
+                        validation_data=batch_generator('./data/IMG/', X_valid, y_valid, args.batch_size, False),
                         nb_val_samples=len(X_valid),
                         callbacks=[checkpoint],
                         verbose=1)
-
-#for command line args
 def s2b(s):
-    """
-    Converts a string to boolean value
-    """
     s = s.lower()
     return s == 'true' or s == 'yes' or s == 'y' or s == '1'
-
-
 def main():
-    """
-    Load train/validation data set and train the model
-    """
     parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
     parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='/data/')
     parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
@@ -144,21 +110,15 @@ def main():
     parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
     args = parser.parse_args()
 
-    #print parameters
     print('-' * 30)
     print('Parameters')
     print('-' * 30)
     for key, value in vars(args).items():
         print('{:<20} := {}'.format(key, value))
     print('-' * 30)
-
-    #load data
     data = load_data(args)
-    #build model
     model = build_model(args)
-    #train model on data, it saves as model.h5 
     train_model(model, args, *data)
-   # model.save('model2.h5')
     
 
 
