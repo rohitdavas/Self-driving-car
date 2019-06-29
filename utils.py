7cc605ade8f669a1872be777d644ecf7d51e996b
Choudhary <rohitchoudhary.git@gmail.com>
2019-06-29 21:44:50 +0530

    v

--- utils.py
+++ utils.py
@@ -7,11 +7,11 @@ IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 
 
-def load_image(data_dir, image_file):
+def load_image(data_dir2, image_file):
     """
     Load RGB images from a file
     """
-    return mpimg.imread(os.path.join(data_dir, image_file.strip()))
+    return mpimg.imread(os.path.join(data_dir2, image_file.strip()))
 
 
 def crop(image):
@@ -45,17 +45,17 @@ def preprocess(image):
     return image
 
 
-def choose_image(data_dir, center, left, right, steering_angle):
+def choose_image(data_dir2, center, left, right, steering_angle):
     """
     Randomly choose an image from the center, left or right, and adjust
     the steering angle.
     """
     choice = np.random.choice(3)
     if choice == 0:
-        return load_image(data_dir, left), steering_angle + 0.2
+        return load_image(data_dir2, left), steering_angle + 0.2
     elif choice == 1:
-        return load_image(data_dir, right), steering_angle - 0.2
-    return load_image(data_dir, center), steering_angle
+        return load_image(data_dir2, right), steering_angle - 0.2
+    return load_image(data_dir2, center), steering_angle
 
 
 def random_flip(image, steering_angle):
@@ -122,12 +122,12 @@ def random_brightness(image):
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
 
-def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y=10):
+def augument(data_dir2, center, left, right, steering_angle, range_x=100, range_y=10):
     """
     Generate an augumented image and adjust steering angle.
     (The steering angle is associated with the center image)
     """
-    image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
+    image, steering_angle = choose_image(data_dir2, center, left, right, steering_angle)
     image, steering_angle = random_flip(image, steering_angle)
     image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
     image = random_shadow(image)
@@ -135,7 +135,7 @@ def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y
     return image, steering_angle
 
 
-def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_training):
+def batch_generator(data_dir2, image_paths, steering_angles, batch_size, is_training):
     """
     Generate training image give image paths and associated steering angles
     """
@@ -148,9 +148,9 @@ def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_train
             steering_angle = steering_angles[index]
             # argumentation
             if is_training and np.random.rand() < 0.6:
-                image, steering_angle = augument(data_dir, center, left, right, steering_angle)
+                image, steering_angle = augument(data_dir2, center, left, right, steering_angle)
             else:
-                image = load_image(data_dir, center) 
+                image = load_image(data_dir2, center) 
             # add the image and steering angle to the batch
             images[i] = preprocess(image)
             steers[i] = steering_angle
