23060974a7f6dd3a6d6dee97e1a00e8f51f5609f
Choudhary <rohitchoudhary.git@gmail.com>
2019-06-29 23:52:38 +0530

    tpu model added

---
+++ On_TPU/model.py
@@ -0,0 +1,138 @@
+import pandas as pd # data analysis toolkit - create, read, update, delete datasets
+import numpy as np #matrix math
+import tensorflow as tf
+#import keras
+from sklearn.model_selection import train_test_split #to split out training and testing data 
+#keras is a high level wrapper on top of tensorflow (machine learning library)
+#The Sequential container is a linear stack of layers
+from keras.models import Sequential
+#popular optimization strategy that uses gradient descent 
+from keras.optimizers import Adam
+#to save our model periodically as checkpoints for loading later
+from keras.callbacks import ModelCheckpoint
+#what types of layers do we want our model to have?
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+#helper class to define input shape and generate training images given image paths & steering angles
+from utils import INPUT_SHAPE, batch_generator
+#for command line arguments
+import argparse
+#for reading files
+#import os
+
+#for debugging, allows for reproducible (deterministic) results 
+np.random.seed(0)
+IMG_path = './data/IMG'
+CSV_path = './data/driving_log.csv'
+
+def load_data(args):
+    """
+    Load training data and split it into training and validation set
+    """
+    #reads CSV file into a single dataframe variable
+    data_df = pd.read_csv(CSV_path, names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+
+    #yay dataframes, we can select rows and columns by their names
+    #we'll store the camera images as our input data
+    X = data_df[['center', 'left', 'right']].values
+    #and our steering commands as our output data
+    y = data_df['steering'].values
+
+    #now we can split the data into a training (80), testing(20), and validation set
+    #thanks scikit learn
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
+
+    return X_train, X_valid, y_train, y_valid
+
+
+def build_model(args):
+    """
+    NVIDIA model used
+    Image normalization to avoid saturation and make gradients work better.
+    Convolution: 5x5, filter: 24, strides: 2x2, activation: ELU
+    Convolution: 5x5, filter: 36, strides: 2x2, activation: ELU
+    Convolution: 5x5, filter: 48, strides: 2x2, activation: ELU
+    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
+    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
+    Drop out (0.5)
+    Fully connected: neurons: 100, activation: ELU
+    Fully connected: neurons: 50, activation: ELU
+    Fully connected: neurons: 10, activation: ELU
+    Fully connected: neurons: 1 (output)
+
+    # the convolution layers are meant to handle feature engineering
+    the fully connected layer for predicting the steering angle.
+    dropout avoids overfitting
+    ELU(Exponential linear unit) function takes care of the Vanishing gradient problem. 
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
+    model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Dropout(args.keep_prob))
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+    model.summary()
+
+    return model
+
+
+def train_model(model, args, X_train, X_valid, y_train, y_valid):
+    import os
+    resolver = tf.contrib.cluster_resolver.TPUClusterResolver('grpc://' + os.environ['COLAB_TPU_ADDR'])
+    tf.contrib.distribute.initialize_tpu_system(resolver)
+    strategy = tf.contrib.distribute.TPUStrategy(resolver)
+
+    with strategy.scope():  
+        checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
+                                 monitor='val_loss',
+                                 verbose=0,
+                                 save_best_only=args.save_best_only,
+                                 mode='auto')
+
+        model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
+
+        model.fit_generator(batch_generator(data_dir2 = IMG_path, image_paths = X_train,steering_angles = y_train,batch_size = args.batch_size, is_training = True),
+                        args.samples_per_epoch,
+                        args.nb_epoch,
+                        max_q_size=1,
+                        validation_data=batch_generator(data_dir2 = IMG_path,image_paths = X_valid, steering_angles =  y_valid, batch_size = args.batch_size, is_training =  False),
+                        nb_val_samples=len(X_valid),
+                        callbacks=[checkpoint],
+                        verbose=1)
+
+def s2b(s):
+    s = s.lower()
+    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
+def main():
+    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='./data/')
+    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
+    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
+    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
+    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
+    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
+    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
+    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
+    args = parser.parse_args()
+
+    print('-' * 30)
+    print('Parameters')
+    print('-' * 30)
+    for key, value in vars(args).items():
+        print('{:<20} := {}'.format(key, value))
+    print('-' * 30)
+    data = load_data(args)
+    model = build_model(args)
+    train_model(model, args, *data)
+    
+
+
+if __name__ == '__main__':
+    main()
+
